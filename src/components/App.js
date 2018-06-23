bd4452fb4970dbeffa0a39b267a1a2fc6bc965aa
Małgorzata Nowak <goskanowak87@gmail.com>
2018-06-23 08:51:02 +0200

    Add configuration webpack

--- src/components/App.js
+++ src/components/App.js
@@ -25,7 +25,7 @@ class App extends React.Component {
   render() {
     return (
       <div className={style.TodoApp}>
-        <Title>Title Todo</Title>
+        <Title title='List Todo' numberTask={this.state.data} />
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,21 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
+module.exports = (env) => { 
+  const environment = env || 'production';
+    
+  return {
+    mode: environment,
+    entry: './src/index.js',
     output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'app.bundle.js'
-  },
+      path: path.resolve(__dirname, 'build'),
+      filename: 'app.' + environment + '.bundle.js'
+    },
+    
   module: {
     rules: [
       {
         test: /\.js$/,
         loader: "babel-loader",
-        options: {
-          presets: ['env']
-        }
       },
       {
         test: /\.css$/,
@@ -29,4 +31,5 @@ module.exports = {
       }
     ]
   }
-};
+}
+}
